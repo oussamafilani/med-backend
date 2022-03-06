7731ac382ee4c7b0f9f866fa691e6aa4bbb33176
Oussama Filani <42185573+filani07@users.noreply.github.com>
2022-03-06 11:46:38 +0100

    fix:fix api structure folder

--- test.js
+++
@@ -1,9 +0,0 @@
-function multiply(a, b, c) {
-    if (typeof c !== 'undefined') {
-        return a * b * c;
-    }
-    return a * b;
-}
-console.log(multiply(2, 4));
-
-console.log(process.argv)
--- test.ts
+++
@@ -1,8 +0,0 @@
-function multiply(a: number, b: number, c?: number): number {
-    if (typeof c !== 'undefined') {
-        return a * b * c;
-    }
-    return a * b;
-}
-
-console.log(multiply(2, 4));
